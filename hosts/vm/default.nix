f575fd44154c6722147507299d4d5305b7a4214b
STYLEY <ssstyley@gmail.com>
2023-02-21 09:49:24 -0500

    fixed laptop boot

--- hosts/vm/default.nix
+++ hosts/vm/default.nix
@@ -3,4 +3,8 @@
   imports = [
     ./hardware-configuration.nix
   ];
+  # Bootloader.
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+  boot.loader.efi.efiSysMountPoint = "/boot/efi";
 }
