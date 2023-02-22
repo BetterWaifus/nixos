daff6ab74a332b2fc4ac8d6fac11b6e223abe170
STYLEY <ssstyley@gmail.com>
2023-02-21 21:23:04 -0500

    undo clean

--- hosts/default.nix
+++ hosts/default.nix
@@ -59,18 +59,18 @@ config = {
         };
 
         extraConfig = {"
-          set_from_resource $fg i3wm.color7 #f0f0f0
-          set_from_resource $bg i3wm.color2 #f0f0f0
+         set_from_resource $fg i3wm.color7 #f0f0f0
+set_from_resource $bg i3wm.color2 #f0f0f0
 
-          # class                 border  backgr. text indicator child_border
-          client.focused          $bg     $bg     $fg  $bg       $bg
-          client.focused_inactive $bg     $bg     $fg  $bg       $bg
-          client.unfocused        $bg     $bg     $fg  $bg       $bg
-          client.urgent           $bg     $bg     $fg  $bg       $bg
-          client.placeholder      $bg     $bg     $fg  $bg       $bg
+# class                 border  backgr. text indicator child_border
+client.focused          $bg     $bg     $fg  $bg       $bg
+client.focused_inactive $bg     $bg     $fg  $bg       $bg
+client.unfocused        $bg     $bg     $fg  $bg       $bg
+client.urgent           $bg     $bg     $fg  $bg       $bg
+client.placeholder      $bg     $bg     $fg  $bg       $bg
 
-          client.background       $bg
-        "};
+client.background       $bg
+"};
         startup = [
           {
             command = "systemctl --user start picom.service";
