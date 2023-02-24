de5a65c19d3459a74785e22a3eab52de995f588d
STYLEY <ssstyley@gmail.com>
2023-02-23 19:58:56 -0500

    added grub to desktop config

--- hosts/desktop/default.nix
+++ hosts/desktop/default.nix
@@ -3,4 +3,28 @@
   imports = [
     ./hardware-configuration.nix
   ];
+
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+  boot.loader.efi.efiSysMountPoint = "/boot";
+
+  home-manager.users.${user} = {
+    xsession.windowManager.i3 = {
+      config = {
+        startup = [
+          {
+            command = "xrandr --output DVI-D-0 --off --output HDMI-0 --off --output DP-0 --off --output DP-1 --off --output DP-2 --mode 1920x1080 --pos 0x0 --rotate left --rate 144 --output DP-3 --off --output DP-4 --primary --mode 2560x1440 --pos 1080x240 --rotate normal --rate 165 --output DP-5 --off";
+            always = true;
+            notification = false;
+          }
+        ]
+      ;
+    };
+  };
+};
+
+ # Enables Nvidia drivers
+  services.xserver.videoDrivers = [ "nvidia" ];
+  hardware.opengl.enable = true;
+
 }
