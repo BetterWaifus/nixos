5f63cf2c5228705c35847305e423f5ed4fde6430
STYLEY <ssstyley@gmail.com>
2023-02-24 00:16:52 -0500

    removed old shit for unstable change

--- hosts/configuration.nix
+++ hosts/configuration.nix
@@ -55,9 +55,6 @@
     LC_TELEPHONE = "en_US.UTF-8";
     LC_TIME = "en_US.UTF-8";
   };
-
-  # Sets to nixos-unstable
-  package = pkgs.nixVersions.unstable;
   
   # Enable the X11 windowing system.
   services.xserver.enable = true;
