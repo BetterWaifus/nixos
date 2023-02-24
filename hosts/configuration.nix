57ad0658b50585a243caa9afed776ece07135af3
STYLEY <ssstyley@gmail.com>
2023-02-23 22:59:52 -0500

    fixed syntax

--- hosts/configuration.nix
+++ hosts/configuration.nix
@@ -170,7 +170,7 @@
   # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
   system.stateVersion = "22.11"; # Did you read the comment?
   
-     # enable flakes
+  # enable flakes
   nix = {
     settings = {
       auto-optimise-store = true; # Optimise syslinks
