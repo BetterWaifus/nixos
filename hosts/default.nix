49cb1d4e304692441936a20af0d0340f2f52183b
STYLEY <ssstyley@gmail.com>
2023-02-24 20:57:02 -0500

    added pywal-discord

--- hosts/default.nix
+++ hosts/default.nix
@@ -83,7 +83,17 @@ config = {
             notification = false;
           }
           {
+            command = lib.mkAfter "exec ${../modules/scripts/set-config.sh}";
+            always = true;
+            notification = false;
+          }
+          {
             command = lib.mkAfter "exec --no-startup-id betterdiscordctl -f canary install";
+            always = true;
+            notification = false;
+          }
+          {
+            command = lib.mkAfter "exec ${../modules/desktop/pywal/pywal-discord/install}";
             always = true;
             notification = false;
           }
