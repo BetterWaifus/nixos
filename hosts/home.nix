99f25309cbfac79949e7012663fb05c8c00b1016
STYLEY <ssstyley@gmail.com>
2023-02-26 06:15:59 -0500

    added keyring.nix

--- hosts/home.nix
+++ hosts/home.nix
@@ -14,6 +14,7 @@
     ../modules/programs/steam.nix
     ../modules/programs/git.nix
     ../modules/programs/heroic.nix
+    ../modules/programs/keyring.nix
   ];
 
   home-manager.users.${user} = {
