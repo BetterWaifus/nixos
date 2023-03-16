153b834a49e914e3a5f50e5e8928d418cc8ce07b
STYLEY <ssstyley@gmail.com>
2023-03-15 22:24:53 -0400

    added autostart to desktop for discord and firefox

--- hosts/desktop/default.nix
+++ hosts/desktop/default.nix
@@ -27,6 +27,9 @@
 
       xsession.windowManager.i3 = {
         config = {
+          assigns = {
+            "1" = [{ class = "^firefox$"; } { class = "^discord$"; }];
+          };
           startup = [
             {
               command = "xrandr --output DP-0 --off --output DP-1 --off --output HDMI-0 --off --output DP-2 --primary --mode 2560x1440 --pos 1080x198 --rotate normal --rate 165 --output DP-3 --off --output DP-4 --mode 1920x1080 --pos 0x0 --rotate left --rate 144 --output DP-5 --off --output USB-C-0 --off";
@@ -40,6 +43,16 @@
             }
             {
               command = "wal -c && wal -i ${../../modules/desktop/wallpaper/desktop}";
+              always = true;
+              notification = false;
+            }
+            {
+              command = lib.mkAfter "exec firefox";
+              always = true;
+              notification = false;
+            }
+            {
+              command = lib.mkAfter "exec discord";
               always = true;
               notification = false;
             }
