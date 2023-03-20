42d48629b296155c215c41c9eee7e60054df384f
STYLEY <ssstyley@gmail.com>
2023-03-20 02:23:18 -0400

    changed colors

--- modules/desktop/hyprland/wallpaper.nix
+++ modules/desktop/hyprland/wallpaper.nix
@@ -7,7 +7,7 @@ let
   hypr-colors = pkgs.writeShellScriptBin "hypr-colors" ''
     source ~/.cache/wal/colors.sh
 
-    hyprctl keyword general:col.active_border "rgba(''${color4:1:10}ee) rgba(''${color5:1:10}ee) 45deg"
+    hyprctl keyword general:col.active_border "rgba(''${color1:1:10}ee) rgba(''${color5:2:10}ee) 45deg"
     hyprctl keyword general:col.inactive_border "rgba(''${color0:1:10}ee)"
     hyprctl keyword windowrulev2 bordercolor "rgba(''${color6:1:10}ff),fullscreen:1" 
     hyprctl keyword windowrulev2 bordercolor "rgba(''${color9:1:10}ff),floating:1" 
